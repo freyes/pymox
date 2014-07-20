7f1677fdba014b6a300e8d87238550f662a2954a
Felipe Reyes <freyes@tty.cl>
2014-07-20 18:55:43 +0200

    mox_test: fix import error of cStringIO

--- mox_test.py
+++ mox_test.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cStringIO
+from six.moves import cStringIO
 import unittest
 import re
 import sys
